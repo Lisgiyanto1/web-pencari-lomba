0918e9303b648598afd1017c0986ebabb2db4e0f
rizkydprnm <>
2023-11-03 14:15:44 +0700

    feat: tes create & read

--- controllers/page.js
+++ controllers/page.js
@@ -1,6 +1,23 @@
+// Model
+const user = require("../models/user");
+
 // Controller
-function index(req, res) {
-    res.send("Hello Minna-sama!");
+async function index(req, res) {
+    try {
+        // Operasi CRUD disini
+        await user.create({
+            username: "hello",
+            password: "minnasama",
+            is_admin: false
+        });
+    }
+    catch (error) {
+
+    }
+
+    // Buat lihat balik datanya
+    let data = await User.findAll();
+    res.status(200).json(data);
 }
 
 module.exports = {
