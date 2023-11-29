35f3f59c38ac21b9e064b2328ecf5782971cf4fe
Rizky Dwi Purnama <rizkypurnama2002@gmail.com>
2023-11-29 19:49:19 +0700

    fix: perbaikan konflik merge model dosen

--- models/dosen.js
+++ models/dosen.js
@@ -2,45 +2,26 @@ import { DataTypes } from 'sequelize';
 import db from '../database.js';
 
 // Model
-<<<<<<< HEAD
-const dosen = db.define(
-  'dosen',
-  {
-    nip: {
-      type: DataTypes.STRING(18),
-      primaryKey: true,
-      allowNull: false,
-=======
 const dosen = db.define('dosen',
     {
         nip: {
             type: DataTypes.STRING(18),
             primaryKey: true,
-            allowNull: false
+            allowNull: false,
         },
         nama: {
             type: DataTypes.STRING(128),
-            allowNull: false
+            allowNull: false,
         },
         kontak: {
             type: DataTypes.STRING(16),
-            allowNull: false
-        }
->>>>>>> 35f33b3cb73b8ef9447328f72bffdbf7dbdad286
+            allowNull: false,
+        },
     },
-    nama: {
-      type: DataTypes.STRING(128),
-      allowNull: false,
-    },
-    kontak: {
-      type: DataTypes.STRING(16),
-      allowNull: false,
-    },
-  },
-  {
-    freezeTableName: true,
-    timestamps: false,
-  }
+    {
+        freezeTableName: true,
+        timestamps: false,
+    }
 );
 
 export default dosen;
